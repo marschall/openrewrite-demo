1c0c253b347f9aa62b280f8f19255231acf64556
Philippe Marschall <philippe.marschall@gmail.com>
2022-09-16 12:16:07 +0200

    bump

---
+++ rewrite.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn rewrite:run
---
+++ src/main/java/com/github/marschall/openrewirte/demo/SampleClass.java
@@ -0,0 +1,8 @@
+package com.github.marschall.openrewirte.demo;
+
+public class SampleClass {
+  
+  private static final String CONSTANT = "A\n"
+      + "B";
+
+}
